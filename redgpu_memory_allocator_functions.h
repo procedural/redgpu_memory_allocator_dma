7066c09934fd20f1da1fabc3c3c39312a85cb2f7
Constantine Tarasenkov <iamvfx@gmail.com>
2021-10-04 22:52:01 +0300

    Pass REDGPU context and GPU index parameters from DMA to RMA

--- redgpu_memory_allocator_functions.h
+++ redgpu_memory_allocator_functions.h
@@ -21,23 +21,23 @@
 extern "C" {
 #endif
 
-REDGPU_DECLSPEC void     REDGPU_API rmaDmaVkGetPhysicalDeviceProperties2      (VkPhysicalDevice physicalDevice, VkPhysicalDeviceProperties2 * pProperties);
-REDGPU_DECLSPEC void     REDGPU_API rmaDmaVkGetPhysicalDeviceMemoryProperties (VkPhysicalDevice physicalDevice, VkPhysicalDeviceMemoryProperties * pMemoryProperties);
-REDGPU_DECLSPEC VkResult REDGPU_API rmaDmaVkAllocateMemory                    (VkDevice device, const VkMemoryAllocateInfo * pAllocateInfo, const VkAllocationCallbacks * pAllocator, VkDeviceMemory * pMemory, const char * name);
-REDGPU_DECLSPEC void     REDGPU_API rmaDmaVkFreeMemory                        (VkDevice device, VkDeviceMemory memory, const VkAllocationCallbacks * pAllocator);
-REDGPU_DECLSPEC VkResult REDGPU_API rmaDmaVkMapMemory                         (VkDevice device, VkDeviceMemory memory, VkDeviceSize offset, VkDeviceSize size, VkMemoryMapFlags flags, void ** ppData);
-REDGPU_DECLSPEC void     REDGPU_API rmaDmaVkUnmapMemory                       (VkDevice device, VkDeviceMemory memory);
-REDGPU_DECLSPEC VkResult REDGPU_API rmaDmaVkBindBufferMemory2                 (VkDevice device, uint32_t bindInfoCount, const VkBindBufferMemoryInfo * pBindInfos);
-REDGPU_DECLSPEC VkResult REDGPU_API rmaDmaVkBindImageMemory2                  (VkDevice device, uint32_t bindInfoCount, const VkBindImageMemoryInfo * pBindInfos);
-REDGPU_DECLSPEC void     REDGPU_API rmaDmaVkGetBufferMemoryRequirements2      (VkDevice device, const VkBufferMemoryRequirementsInfo2 * pInfo, VkMemoryRequirements2 * pMemoryRequirements);
-REDGPU_DECLSPEC void     REDGPU_API rmaDmaVkGetImageMemoryRequirements2       (VkDevice device, const VkImageMemoryRequirementsInfo2 * pInfo, VkMemoryRequirements2 * pMemoryRequirements);
-REDGPU_DECLSPEC VkResult REDGPU_API rmaDmaVkCreateBuffer                      (VkDevice device, const VkBufferCreateInfo * pCreateInfo, const VkAllocationCallbacks * pAllocator, VkBuffer * pBuffer);
-REDGPU_DECLSPEC void     REDGPU_API rmaDmaVkDestroyBuffer                     (VkDevice device, VkBuffer buffer, const VkAllocationCallbacks * pAllocator);
-REDGPU_DECLSPEC VkResult REDGPU_API rmaDmaVkCreateImage                       (VkDevice device, const VkImageCreateInfo * pCreateInfo, const VkAllocationCallbacks * pAllocator, VkImage * pImage);
-REDGPU_DECLSPEC void     REDGPU_API rmaDmaVkDestroyImage                      (VkDevice device, VkImage image, const VkAllocationCallbacks * pAllocator);
-REDGPU_DECLSPEC void     REDGPU_API rmaDmaVkCmdInsertDebugUtilsLabelEXT       (VkCommandBuffer commandBuffer, const VkDebugUtilsLabelEXT * pLabelInfo);
-REDGPU_DECLSPEC void     REDGPU_API rmaDmaVkCmdBeginDebugUtilsLabelEXT        (VkCommandBuffer commandBuffer, const VkDebugUtilsLabelEXT * pLabelInfo);
-REDGPU_DECLSPEC void     REDGPU_API rmaDmaVkCmdEndDebugUtilsLabelEXT          (VkCommandBuffer commandBuffer);
+REDGPU_DECLSPEC void     REDGPU_API rmaDmaVkGetPhysicalDeviceProperties2      (RedContext context, unsigned gpuIndex, VkPhysicalDevice physicalDevice, VkPhysicalDeviceProperties2 * pProperties);
+REDGPU_DECLSPEC void     REDGPU_API rmaDmaVkGetPhysicalDeviceMemoryProperties (RedContext context, unsigned gpuIndex, VkPhysicalDevice physicalDevice, VkPhysicalDeviceMemoryProperties * pMemoryProperties);
+REDGPU_DECLSPEC VkResult REDGPU_API rmaDmaVkAllocateMemory                    (RedContext context, unsigned gpuIndex, VkDevice device, const VkMemoryAllocateInfo * pAllocateInfo, const VkAllocationCallbacks * pAllocator, VkDeviceMemory * pMemory, const char * name);
+REDGPU_DECLSPEC void     REDGPU_API rmaDmaVkFreeMemory                        (RedContext context, unsigned gpuIndex, VkDevice device, VkDeviceMemory memory, const VkAllocationCallbacks * pAllocator);
+REDGPU_DECLSPEC VkResult REDGPU_API rmaDmaVkMapMemory                         (RedContext context, unsigned gpuIndex, VkDevice device, VkDeviceMemory memory, VkDeviceSize offset, VkDeviceSize size, VkMemoryMapFlags flags, void ** ppData);
+REDGPU_DECLSPEC void     REDGPU_API rmaDmaVkUnmapMemory                       (RedContext context, unsigned gpuIndex, VkDevice device, VkDeviceMemory memory);
+REDGPU_DECLSPEC VkResult REDGPU_API rmaDmaVkBindBufferMemory2                 (RedContext context, unsigned gpuIndex, VkDevice device, uint32_t bindInfoCount, const VkBindBufferMemoryInfo * pBindInfos);
+REDGPU_DECLSPEC VkResult REDGPU_API rmaDmaVkBindImageMemory2                  (RedContext context, unsigned gpuIndex, VkDevice device, uint32_t bindInfoCount, const VkBindImageMemoryInfo * pBindInfos);
+REDGPU_DECLSPEC void     REDGPU_API rmaDmaVkGetBufferMemoryRequirements2      (RedContext context, unsigned gpuIndex, VkDevice device, const VkBufferMemoryRequirementsInfo2 * pInfo, VkMemoryRequirements2 * pMemoryRequirements);
+REDGPU_DECLSPEC void     REDGPU_API rmaDmaVkGetImageMemoryRequirements2       (RedContext context, unsigned gpuIndex, VkDevice device, const VkImageMemoryRequirementsInfo2 * pInfo, VkMemoryRequirements2 * pMemoryRequirements);
+REDGPU_DECLSPEC VkResult REDGPU_API rmaDmaVkCreateBuffer                      (RedContext context, unsigned gpuIndex, VkDevice device, const VkBufferCreateInfo * pCreateInfo, const VkAllocationCallbacks * pAllocator, VkBuffer * pBuffer);
+REDGPU_DECLSPEC void     REDGPU_API rmaDmaVkDestroyBuffer                     (RedContext context, unsigned gpuIndex, VkDevice device, VkBuffer buffer, const VkAllocationCallbacks * pAllocator);
+REDGPU_DECLSPEC VkResult REDGPU_API rmaDmaVkCreateImage                       (RedContext context, unsigned gpuIndex, VkDevice device, const VkImageCreateInfo * pCreateInfo, const VkAllocationCallbacks * pAllocator, VkImage * pImage);
+REDGPU_DECLSPEC void     REDGPU_API rmaDmaVkDestroyImage                      (RedContext context, unsigned gpuIndex, VkDevice device, VkImage image, const VkAllocationCallbacks * pAllocator);
+REDGPU_DECLSPEC void     REDGPU_API rmaDmaVkCmdInsertDebugUtilsLabelEXT       (RedContext context, unsigned gpuIndex, VkCommandBuffer commandBuffer, const VkDebugUtilsLabelEXT * pLabelInfo);
+REDGPU_DECLSPEC void     REDGPU_API rmaDmaVkCmdBeginDebugUtilsLabelEXT        (RedContext context, unsigned gpuIndex, VkCommandBuffer commandBuffer, const VkDebugUtilsLabelEXT * pLabelInfo);
+REDGPU_DECLSPEC void     REDGPU_API rmaDmaVkCmdEndDebugUtilsLabelEXT          (RedContext context, unsigned gpuIndex, VkCommandBuffer commandBuffer);
 
 #ifdef __cplusplus
 }
